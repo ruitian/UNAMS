a0d81d054e723b621a8d5d934d62773f4f9e331a
ruitian <chinabingwei@163.com>
2015-12-21 21:44:12 +0800

    modify unit

--- app/models/unit.py
+++ app/models/unit.py
@@ -16,10 +16,24 @@ class UnitModel(db.Model):
         server_default=db.func.current_timestamp()
     )
     teachers = db.relationship('UserModel', backref='unit', lazy='dynamic')
-    major = db.relationship('MajorModel', backref='unit', lazy='dynamic')
+    major = db.relationship('MajorModel', backref='major', lazy='dynamic')
+    students = db.relationship('Student', backref='unit', lazy='dynamic')
 
     def __repr__(self):
         return self.unit_name
+
+    def department_to_json(self):
+        return {
+            'id': self.id,
+            'acachemy': {
+                'acachemy_id': self.unit_id,
+                'acachemy_name': self.unit_name
+            },
+            'majors': [{
+                'major_id': major.id,
+                'major_name': major.major_name
+            }for major in self.major]
+        }
 
 
 class MajorModel(db.Model):
@@ -32,3 +46,4 @@ class MajorModel(db.Model):
         server_default=db.func.current_timestamp()
     )
     id_unit = db.Column(db.Integer, db.ForeignKey('unit.id'))
+    students = db.relationship('Student', backref='major', lazy='dynamic')
