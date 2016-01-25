a8c743770bebe27dd7184c769074b343149acf0d
ruitian <chinabingwei@163.com>
2016-01-25 15:20:36 +0800

    论文表单

---
+++ app/forms/paper/paper.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from flask.ext.wtf import Form
+from wtforms import SubmitField, StringField, DateTimeField
+from wtforms.validators import Required
+
+
+class PaperForm(Form):
+    paper_title = StringField(u'论文题目', validators=[Required()])
+    issn = StringField(u'刊号', validators=[Required()])
+    precedence = StringField(u'位次', validators=[Required()])
+    periodical = StringField(u'刊物名称', validators=[Required()])
+    public = StringField(u'发表时间', validators=[Required()])
+    student_name = StringField(u'作者', validators=[Required()])
+    academy = StringField(u'学院', validators=[Required()])
+    grade = StringField(u'班级', validators=[Required()])
