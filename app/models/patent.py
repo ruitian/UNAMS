3b61bf8b7a97fb079b0d311536898a0000caf803
ruitian <chinabingwei@163.com>
2016-02-17 11:47:31 +0800

    patent model

--- app/models/patent.py
+++ app/models/patent.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from app import app, db
+from wtforms import widgets, fields
+from wtforms.ext.sqlalchemy.orm import model_form
 
 
 class PatentModel(db.Model):
@@ -7,7 +9,10 @@ class PatentModel(db.Model):
     __tablename__ = 'patent'
     id = db.Column(db.Integer, primary_key=True)
     patent_title = db.Column(db.String(256), nullable=False)
-    category = db.Column(db.String(256), nullable=False)
+    category = db.Column(db.Enum(
+        *app.config['PATENT_CATEGORY'],
+        name='patent_category_enum'
+    ))
     student_name = db.Column(db.String(64))
     student_class = db.Column(db.String(64))
     precedence = db.Column(db.String(128))
@@ -19,3 +24,13 @@ class PatentModel(db.Model):
 
     def __repr__(self):
         return self.patent_title
+
+    @classmethod
+    def model_form(cls, *args, **kwargs):
+        return model_form(
+            model=cls,
+            db_session=db.session,
+            field_args={
+                'category': {'widget': widgets.Select()}
+            }
+        )
