d4e89d8fcf56f65b6b782ed3405a527592366713
ruitian <chinabingwei@163.com>
2016-02-18 09:23:32 +0800

    修改view

--- app/views/paper/index.py
+++ app/views/paper/index.py
@@ -1,29 +1,22 @@
 # -*- coding: utf-8 -*-
-from ...forms import PaperForm
 from ...models import PaperModel
 from app import app, db
 from flask.ext.login import login_required
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, flash
 
 
 @app.route('/paper', methods=['GET', 'POST'])
 @login_required
 def paper():
-    form = PaperForm()
-    if request.method == 'POST' and form.validate_on_submit:
-        title = form.public.data
-        paper = PaperModel(
-            paper_title=form.paper_title.data,
-            periodical=form.periodical.data,
-            level=form.level.data,
-            precedence=form.precedence.data,
-            issn=form.issn.data,
-            student_name=form.student_name.data,
-            student_class=form.grade.data,
-            acachemy=form.acachemy.data,
-            public=form.public.data
-        )
+    PaperForm = PaperModel.model_form()
+    form = PaperForm(request.form)
+    if request.method == 'POST':
+        if not form.validate():
+            return render_template('paper/paper.html', form=form)
+        paper = PaperModel()
+        form.populate_obj(paper)
         db.session.add(paper)
         db.session.commit()
-        return redirect('/')
+        flash(u'添加信息成功！')
+        return redirect('/paper')
     return render_template('paper/paper.html', form=form)
