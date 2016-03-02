b8c408a35940c970871950cb00356d4fe4d5e308
ruitian <chinabingwei@163.com>
2016-03-02 15:47:41 +0800

    处理首页跳转

--- app/views/index.py
+++ app/views/index.py
@@ -2,11 +2,12 @@
 from flask import render_template, redirect, url_for
 from app import app
 from .. decorators import permission_required
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
 from .. models import Permission
 
 
 @app.route('/')
-@login_required
 def index():
-    return redirect(url_for('competition'))
+    if current_user.is_authenticated():
+        return redirect(url_for('competition'))
+    return redirect(url_for('login'))
