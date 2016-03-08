cdf5f0a8870fa6cf269306f7cf8874ebb72aeb4d
ruitian <chinabingwei@163.com>
2016-03-08 09:55:31 +0800

    上传专利附近

--- app/views/patent/index.py
+++ app/views/patent/index.py
@@ -5,6 +5,16 @@ from app import app, db
 from flask.ext.login import login_required
 from flask import render_template, request, redirect, flash
 
+from werkzeug import secure_filename
+import os
+
+ALLOWED_EXTENSIONS = set(['doc', 'xls'])
+UPLOAD_FOLDER = app.config['UPLOAD_FOLDER']
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 @app.route('/patent', methods=['GET', 'POST'])
 @login_required
@@ -18,6 +28,13 @@ def patent():
         form.populate_obj(patent)
         db.session.add(patent)
         db.session.commit()
+        files = request.files['annex']
+        if files and allowed_file(files.filename):
+            print files.filename
+            filename = 'patent_%d.doc' % patent.id
+            file_url = os.path.join(
+                app.config['UPLOAD_FOLDER']+'/patent', filename)
+            files.save(file_url)
         flash(u'添加成功!')
         return redirect('/patent')
     return render_template('patent/patent.html', form=form)
